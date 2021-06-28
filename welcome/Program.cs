e792bdea8e60e0069dbb40535ae8c08ed7d24e07
unknown <sivaranjanib99@gmail.com>
2021-06-28 18:56:58 +0530

    added uc1 prgm

--- welcome/Program.cs
+++ welcome/Program.cs
@@ -7,7 +7,18 @@ namespace welcome
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
-           
+            int employeepresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+            if(empInput == employeepresent) 
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.Read();
         }
     }
 }
