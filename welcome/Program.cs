d5bd5104bf8a435e707ebada34c3ff5e2ce66f33
unknown <sivaranjanib99@gmail.com>
2021-06-28 20:22:28 +0530

    added uc5 prgm

--- welcome/Program.cs
+++ welcome/Program.cs
@@ -7,30 +7,36 @@ namespace welcome
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
+        public const int WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
-            
+
             int empHours = 0;
             int empWages = 0;
+            int totalWage = 0;
 
             Random random = new Random();
-            int empInput = random.Next(0, 3);
-            switch(empInput)
+            for (int day = 1; day <= WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    empHours = 8;
-                    break;
-                case PART_TIME:
-                    empHours = 4;
-                    break;
-                default:
-                    empHours = 0;
-                    break;
-            }
-            
-            empWages = EMP_RATE_PER_HR * empHours;
-            Console.WriteLine("Employee wage per day = " +empWages);
+                int empInput = random.Next(0, 3);
+                switch (empInput)
+                {
+                    case FULL_TIME:
+                        empHours = 8;
+                        break;
+                    case PART_TIME:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+
+                empWages = EMP_RATE_PER_HR * empHours;
+                totalWage += empWages;
+            } 
+            Console.WriteLine("Employee wage " + WORKING_DAYS + "day = "+ totalWage);
             Console.Read();
         }
     }
