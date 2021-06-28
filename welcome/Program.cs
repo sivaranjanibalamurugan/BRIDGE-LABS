0210b4b761712d0ef86fdaea9ab1546e69bf79da
unknown <sivaranjanib99@gmail.com>
2021-06-28 12:24:09 +0530

    welcome

--- welcome/Program.cs
+++ welcome/Program.cs
@@ -7,18 +7,7 @@ namespace welcome
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
-            int employeepresent = 1;
-            Random random = new Random();
-            int empInput = random.Next(0, 2);
-                if (empInput == employeepresent)
-                    {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("employee is absent"); 
-            }
-            Console.Read();
+           
         }
     }
 }
