9cb2665697233a634430d9bd2bf281a8e000b58e
unknown <sivaranjanib99@gmail.com>
2021-06-28 11:37:43 +0530

    welcome

---
+++ welcome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
+        }
+    }
+}
