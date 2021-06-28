ccb2b94b96f5ea8c9c37a2ff2f642c7498b135b8
unknown <sivaranjanib99@gmail.com>
2021-06-28 19:19:23 +0530

    added uc2 prgm

--- welcome/Program.cs
+++ welcome/Program.cs
@@ -8,16 +8,24 @@ namespace welcome
         {
             Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
             int employeepresent = 1;
+            int empHours = 0;
+            int empRatePerHour = 20;
+            int empWages = 0;
+
             Random random = new Random();
             int empInput = random.Next(0, 2);
             if(empInput == employeepresent) 
             {
                 Console.WriteLine("Employee is present");
+                empHours = 8;
             }
             else 
             {
                 Console.WriteLine("Employee is absent");
+                empHours = 0;
             }
+            empWages = empRatePerHour * empHours;
+            Console.WriteLine("Employee wage per day = " +empWages);
             Console.Read();
         }
     }
