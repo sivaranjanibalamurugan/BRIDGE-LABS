2e23ee535fd1551705da38f5ed788858e7437d72
unknown <sivaranjanib99@gmail.com>
2021-07-01 22:34:13 +0530

    [siva] computing wages for multiple companies

--- welcome/Program.cs
+++ welcome/Program.cs
@@ -2,29 +2,34 @@
 
 namespace welcome
 {
-    class Program
+    class EmployeeWageBuilder
     {
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
-        public const int EMP_RATE_PER_HR = 20;
-        public const int WORKING_DAYS = 20;
-        public const int MAX_WORKING_HRS = 100;
-        public const int MAX_WORKING_DAYS = 20;
+        //instance variables creating separating for each object
+        public string Company;
+        public int empRatePerHr;
+        public int maxWorkingDays;
+        public int maxWorkingHrs;
+       
         //UC7 Introducing class method 
-         public static void ComputeWage()
+         public static void ComputeWage(string company, int empRatePerHr,int maxWorkingDays, int maxWorkingHrs)
         {
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
+            //Console.WriteLine("WELCOME TO EMPLOYEE WAGES COMPUTATION PROGRAM");
 
             int empHours = 0;
             int empWages = 0;
             int totalWage = 0;
             int workingDays = 1;
             int workingHours = 0;
-
+            //creating object or instance of random class
             Random random = new Random();
-            while (workingDays<= MAX_WORKING_DAYS && workingHours<= MAX_WORKING_HRS)
+            while (workingDays<= maxWorkingDays && workingHours<= maxWorkingHrs)
             {
+                //calling Next() method for generating random values
+               
                 int empInput = random.Next(0, 3);
+                //selection statement
                 switch (empInput)
                 {
                     case FULL_TIME:
@@ -37,20 +42,22 @@ namespace welcome
                         empHours = 0;
                         break;
                 }
-
-                empWages = EMP_RATE_PER_HR * empHours;
+                //applying formula for finding out employee salary
+                empWages = empRatePerHr * empHours;
                 workingHours += empHours;
-                totalWage += empWages;
-                workingDays++;
+                totalWage += empWages;//totalwages =totalWage+empWage;
+                if ( empInput ! = 0)
+                {
+                    workingDays++;
+                }
+           
             }
-            Console.WriteLine("WorkingHrs =" + MAX_WORKING_HRS + "Working days=" + workingDays);
-            Console.WriteLine("Employee wage for " + MAX_WORKING_DAYS + "days = "+ totalWage);
-
-            
+            //Console.WriteLine("WorkingHrs = {0} Working days = {1}" , workingHours, workingDays);
+            Console.WriteLine("Employee wage for {0} is {1}" ,company  ,totalWage);
          }
         static void Main(string[] args)
         {
-            ComputeWage();
+            ComputeWage("Deloitee", 50,18,70);
             Console.Read();
         }
     }
