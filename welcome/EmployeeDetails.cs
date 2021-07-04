9241662c5274761f4b6a35d526f70e11582299c1
unknown <sivaranjanib99@gmail.com>
2021-07-04 22:17:11 +0530

    [siva] storing dailywages and dailywages

--- welcome/EmployeeDetails.cs
+++ welcome/EmployeeDetails.cs
@@ -14,6 +14,7 @@ namespace welcome
         public int maxWorkingDays;
         public int maxWorkingHrs;
         private int totalWages;
+        private LinkedList<int> dailywage;
 
         //Assign the values passed during object creation to current variables
         public EmployeeDetails(string companyName, int employeeRatePerHr, int maxWorkingDays, int maxWorkingHrs)
@@ -22,6 +23,7 @@ namespace welcome
             this.employeeRatePerHr = employeeRatePerHr;
             this.maxWorkingDays = maxWorkingDays;
             this.maxWorkingHrs = maxWorkingHrs;
+            dailywage = new LinkedList<int>();
         }
 
         public void SetEmployeeWage(int wage)
@@ -29,11 +31,33 @@ namespace welcome
             this.totalWages = wage;
         }
 
+        //set the daily wages for the employee
+        public void SetDailyWage(int wage, EmployeeDetails employee)
+        {
+            employee.dailywage.AddLast(wage);
+        }
+
         public string toString()
         {
             return "Total Employee wage for company \"" + this.companyName + "\" is :" + this.totalWages;
         }
 
+        //display the daily wages for the employee 
+        public void DisplayDailyWage(EmployeeDetails employee)
+        {
+            int day = 1;
+            foreach (int wage in employee.dailywage)
+            {
+                Console.WriteLine("Day{0}:{1}", day, wage);
+                day++;
+            }
+        }
+
     }
 }
+
+    
+
+
+    
 
